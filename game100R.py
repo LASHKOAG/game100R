54f291030559c6b8681326800a5873df4b62bc41
LASHKOAG <lashkoag@yandex.ru>
2022-12-04 10:30:57 +0600

    rename var

--- game100R.py
+++ game100R.py
@@ -9,7 +9,7 @@ print ("len(sys.argv) = %d" % len(sys.argv))
 
 for i in range(len(sys.argv)):
     if i == 0:
-        print ("Function name: %s" % sys.argv[0])
+        print ("Program name: %s" % sys.argv[0])
     else:
         print("%d. argument: %s" % (i, sys.argv[i]))
 		
