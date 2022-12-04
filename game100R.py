74d640cbee3737b28772415bfba27f59031f6155
LASHKOAG <lashkoag@yandex.ru>
2022-12-04 10:25:58 +0600

    argv

---
+++ game100R.py
@@ -0,0 +1,16 @@
+
+#get argv from user
+import sys
+
+# название файла
+print(sys.argv)
+
+print ("len(sys.argv) = %d" % len(sys.argv))
+
+for i in range(len(sys.argv)):
+    if i == 0:
+        print ("Function name: %s" % sys.argv[0])
+    else:
+        print("%d. argument: %s" % (i, sys.argv[i]))
+		
+#--------------------------------------------------------------
